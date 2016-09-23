c53aa3bba16958c6a294cb89def0f1e08727ae9c
Daniel Lim <hbt@markhamboards.com>
2016-09-23 10:03:59 -0400

    mid-work
    mid-work

--- app/scripts.js
+++ app/scripts.js
@@ -1,9 +1,40 @@
+class Template extends React.Component {
+  render() {
+    return <div> </div>;
+  }
+  /*
+  componentWillMount: function() {
+    this.serverRequest1 = $.ajax({
+      type: 'GET',
+      url: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent',
+      success: function(data) {
+        let count = 1;
+      }
+    }) */
+}
+
 class LeaderBoard extends React.Component{
   render(){
-    return <h1>FCC LeaderBoard!</h1>
+    return <h1>Build an FCC Camper Leaderboard!</h1>
   }
 }
 
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="container-fluid">
+          <p>By: <a href="http://www.freecodecamp.com/profoundhub" target="_blank">Daniel</a></p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <LeaderBoard />,document.getElementById('leaderboard')
+  <Template />,
+  <LeaderBoard />, document.getElementById('leaderboard'),
+  <Footer />
 );
